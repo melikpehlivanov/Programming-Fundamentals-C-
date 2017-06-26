d9c9675abd5d9520e3bcea1e627a0dbd38f3171e
melikpehlivanov <melikpehlivanov@gmail.com>
2017-06-26 13:34:13 +0300

    Update Program.cs

--- More Exercises - Lists/2. IntegerInsertion/Program.cs
+++ More Exercises - Lists/2. IntegerInsertion/Program.cs
@@ -2,11 +2,25 @@
 {
     using System;
     using System.Linq;
-   public class Program
+   public class InsertingInteger
     {
        public static void Main()
         {
-
+            var input = Console.ReadLine();
+            var list = input.Split(' ').Select(int.Parse).ToList();
+            input = Console.ReadLine();
+ 
+            while (input != "end")
+            {
+ 
+                var currentNum = int.Parse(input);
+                var firstDigit = input[0].ToString();
+                var index = int.Parse(firstDigit);
+ 
+                list.Insert(index, currentNum);
+                input = Console.ReadLine();
+            }
+            Console.WriteLine(string.Join(" ", list));
         }
     }
 }
