20745bf828ca274c5d12707c276a89e3f9068020
melikpehlivanov <melikpehlivanov@gmail.com>
2017-07-15 18:25:32 +0300

    Dictionaries - Lab

---
+++ Dictorinaries - Lab/01. Odd Occurrences/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _01.Odd_Occurrences
+{
+    class Program
+    {
+        static void Main()
+        {
+            string[] words = Console.ReadLine().ToLower().Split(' ');
+            var dictionary = new Dictionary<string, int>();
+
+            for (int i = 0; i < words.Length; i++)
+            {
+                if (!dictionary.ContainsKey(words[i]))
+                {
+                    dictionary.Add(words[i], 0);
+                }
+                dictionary[words[i]]++;
+            }
+
+            var list=new List<string>();
+
+            foreach (var item in dictionary)
+            {
+                
+                if (item.Value % 2 == 1)
+                {
+                    list.Add(item.Key);
+                    
+                }
+            }
+            Console.WriteLine(string.Join(", ", list));
+        }
+    }
+}
---
+++ Dictorinaries - Lab/2. Count Real Numbers/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _2.Count_Real_Numbers
+{
+    class Program
+    {
+        static void Main()
+        {
+            var list = Console.ReadLine().Split(' ').Select(double.Parse).ToList();
+            var sortedDictionary=new SortedDictionary<double, int>();
+
+            for (int i = 0; i < list.Count; i++)
+            {
+                if (!sortedDictionary.ContainsKey(list[i]))
+                {
+                    sortedDictionary.Add(list[i], 0);
+                }
+                sortedDictionary[list[i]]++;
+            }
+
+            foreach (var item in sortedDictionary)
+            {
+                Console.WriteLine($"{item.Key} -> {item.Value}");
+            }
+        }
+    }
+}
