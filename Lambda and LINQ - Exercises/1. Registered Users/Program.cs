dad145ed6ec4bd9b4eab450b5f7ab4b9681cce19
melikpehlivanov <melikpehlivanov@gmail.com>
2017-07-19 16:05:24 +0300

    Updated Registered Users

--- Lambda and LINQ - Exercises/1. Registered Users/Program.cs
+++ Lambda and LINQ - Exercises/1. Registered Users/Program.cs
@@ -5,9 +5,9 @@ using System.Linq;
 
 namespace _1.Registered_Users
 {
-    class Program
+    public class Program
     {
-        static void Main()
+        public static void Main()
         {
             Dictionary<string, DateTime> registeredUsernames = new Dictionary<string, DateTime>();
             string inputLine = Console.ReadLine();
